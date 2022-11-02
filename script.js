de4334cd3802634e8e29ebd0c65fa040de8428b4
kateFrontend <ecdanilova@gmail.com>
2022-11-01 20:17:43 -0400

    add animation

---
+++ script.js
@@ -0,0 +1,3 @@
+gsap.to(".intro_title", { duration: 1, delay: 1, ease: 'power2.inOut', opacity: 1 })
+gsap.to(".btn", { duration: 1, delay: 2, ease: 'power2.inOut', opacity: 1})
+gsap.to(".nav", { duration: 1, delay: 2.5, ease: 'power2.inOut', opacity: 1})
